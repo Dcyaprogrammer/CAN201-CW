ca8613004e0528371cc7fc20f00178c2697df4a9
Dcyaprogrammer <dujustin6@gmail.com>
2025-12-01 23:06:46 +0800

    redirect function

---
+++ CW2/ryu_redirect.py
@@ -0,0 +1,128 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet, ether_types
+from ryu.lib.packet import ipv4, tcp
+
+
+class RyuRedirect(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(RyuRedirect, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    # table-miss rule
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+
+        if buffer_id:
+        mod = parser.OFPFlowMod(datapath=datapath, 
+                                priority=priority, 
+                                match=match, 
+                                instructions=inst, 
+                                buffer_id=buffer_id)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, 
+                                    priority=priority, 
+                                    match=match, 
+                                    instructions=inst)
+        datapath.send_msg(mod)
+    
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes", ev.msg.msg_len, ev.msg.total_len)
+        
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        # ignore lldp packets
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            return
+        
+        # check TCP/IPv4
+        ip_pkt = pkt.get_protocol(ipv4.ipv4)
+        if not ip_pkt:
+            return
+        tcp_pkt = ip_pkt.get_protocol(tcp.tcp)
+        if not tcp_pkt:
+            return
+        
+        # check TCP SYN
+        if not (tcp_pkt.bits & tcp.TCP_SYN) or (tcp_pkt.bits & tcp.TCP_ACK):
+            return
+        
+        src_ip = ip_pkt.src
+        dst_ip = ip_pkt.dst
+        src_port = tcp_pkt.src_port
+        dst_port = tcp_pkt.dst_port
+        
+        # learn mac
+        dst = eth.dst
+        src = eth.src
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+        
+        # Redirect logic: All traffic from Client (03) to Server1 (01) -> redirect to Server2 (02)
+        if src == '00:00:00:00:00:03':  # Client MAC
+            is_to_server1 = (dst == '00:00:00:00:00:01') or (dst_ip == '10.0.1.2')
+            
+            if is_to_server1 and '00:00:00:00:00:02' in self.mac_to_port[dpid]:  # Server2 MAC learned
+                out_port = self.mac_to_port[dpid]['00:00:00:00:00:02']
+                self.logger.info("Redirect: Client traffic to Server1 (dst=%s, dst_ip=%s) redirected to Server2 on port %s", 
+                               dst, dst_ip, out_port)
+        
+        actions = [parser.OFPActionOutput(out_port)]
+
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(
+                in_port=in_port,
+                eth_dst=dst,
+            )
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, buffer_id=msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions, buffer_id=None)
+        
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id, 
+            in_port=in_port,
+            actions=actions,
+            data=data
+        )
+        datapath.send_msg(out)
