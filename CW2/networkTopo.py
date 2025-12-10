e73b592b931da2f1aa26ea09ea643281a05c8641
Justin Du <dopamine@JustindeMac-mini.local>
2025-12-10 21:25:21 +0800

    update topo

--- CW2/networkTopo.py
+++ CW2/networkTopo.py
@@ -6,16 +6,16 @@ from mininet.node import RemoteController
 from mininet.term import makeTerm
 
 def myTopo():
-    net = Mininet(topo=None, autoSetMacs=True, build=False, ipBase='10.0.1.0/24')
+    net = Mininet(topo=None, autoSetMacs=False, build=False, ipBase='10.0.1.0/24')
 
     # add controller
     c1 = net.addController('c1', controller=RemoteController)
 
     # add hosts
-    client = net.addHost('client', cls=Host, ip='10.0.1.5/24', defaultRoute=None)
-    server1 = net.addHost('server1', cls=Host, ip='10.0.1.2/24', defaultRoute=None)
-    server2 = net.addHost('server2', cls=Host, ip='10.0.1.3/24', defaultRoute=None)
-
+    client = net.addHost('client', ip='10.0.1.5/24', mac='00:00:00:00:00:03', cls=Host, defaultRoute=None)
+    server1 = net.addHost('server1', ip='10.0.1.2/24', mac='00:00:00:00:00:01', cls=Host, defaultRoute=None)
+    server2 = net.addHost('server2', ip='10.0.1.3/24', mac='00:00:00:00:00:02', cls=Host, defaultRoute=None)
+    
     # add switch
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch, failMode='standalone')
 
@@ -26,19 +26,11 @@ def myTopo():
 
     net.build()
 
-    # set mac
-    server1.setMAC(intf='se1-eth0', mac='00:00:00:00:00:01')
-    server2.setMAC(intf='se2-eth0', mac='00:00:00:00:00:02')
-    client.setMAC(intf='cl-eth0', mac='00:00:00:00:00:03')
-
-    server1.setIP(intf='se1-eth0', ip='10.0.1.2/24')
-    server2.setIP(intf='se2-eth0', ip='10.0.1.3/24')
-    client.setIP(intf='cl-eth0', ip='10.0.1.5/24')
 
     # start network
     net.start()
 
-    net.terms += [makeTerm(c1), makeTerm(s1), makeTerm(server1), makeTerm(server2), makeTerm(client)]
+    net.terms += [makeTerm(s1), makeTerm(server1), makeTerm(server2), makeTerm(client)]
 
     # run CLI
     CLI(net)
